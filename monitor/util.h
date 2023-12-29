084b00f2a9524c59e04c592dea57f4fccf0c3947
cacobaco <lcjmonizf09@gmail.com>
2023-12-29 22:34:08 +0000

    updated util

--- monitor/util.h
+++ monitor/util.h
@@ -1,9 +1,6 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-extern int fd;
-extern char *ptr;
-extern int nbytes;
 extern int readn(int fd, char *ptr, int nbytes);
 extern int writen(int fd, char *ptr, int nbytes);
 extern int readline(int fd, char *ptr, int maxlen);
